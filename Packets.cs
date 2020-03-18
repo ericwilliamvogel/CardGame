0f239ac5d370659962cacf780d8404a7c45ec8c4
ericwilliamvogel <52148989+ericwilliamvogel@users.noreply.github.com>
2020-03-18 01:37:10 -0700

    Add files via upload

---
+++ Packets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGameServer
+{
+
+        public enum ServerPackets
+        {
+            // Sent from server to client
+            welcome = 1
+        }
+        public enum ClientPackets
+        {
+            // Sent from client to server
+            welcomeReceived = 1,
+        }
+    
+
+}
